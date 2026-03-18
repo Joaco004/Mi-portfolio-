8cdc865450f51f01afbf900b2d35809f4d9b4b3a
Joaco004 <jgarinei@hotmail.com>
2026-03-18 20:47:23 -0300

    feat: creacion del Hero

---
+++ src/components/Hero.jsx
@@ -0,0 +1,28 @@
+import perfil from "../assets/kayne-perfil.jpg"
+import '../styles/Hero.css'
+
+const Hero = () => {
+
+    return(
+        <section className="hero">
+            <div className="hero-content">
+                <h1>Transformo ideas de negocio en <span className="accent">software de alto rendimiento</span></h1>
+                <p className="hero-description">Desarrollador Full Stack apasionado por construir soluciones digitales completas y eficientes. Con un enfoque sólido en React para interfaces de usuario intuitivas y de alto rendimiento, y en Node.js para arquitecturas de backend robustas y escalables. Me especializo en llevar proyectos desde la conceptualización hasta la implementación y el despliegue, combinando la mejor experiencia de usuario con una sólida lógica de servidor. Mi objetivo es crear software que no solo funcione, sino que impulse el éxito del negocio.</p>
+
+                <div className="hero-buttons">
+                <button className="btn-primary">Mis proyectos</button>
+                <button className="btn-secundary">Contactarme</button>
+                </div>
+            </div>
+
+            <div className="hero-img">
+                <img src={perfil} alt="Joaquin Garinei" />
+            </div>
+            
+            
+        </section>
+
+    )
+}
+
+export default Hero;
