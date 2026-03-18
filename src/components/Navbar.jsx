0f42c327088f6e803211b01933ff7d9d095a9000
Joaco004 <jgarinei@hotmail.com>
2026-03-18 17:33:48 -0300

    feat:agrego link de Linkedin al titulo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
         <nav className="navbar">
             <div className="navbar-logo">
                 <img src={logo} alt="logo" />
-                <h1> Joaquin Garinei <span className="accent">Full Stack Developer</span></h1>
+                <h1><a href="https://www.linkedin.com/in/joaquin-garinei-892654304/">Joaquin Garinei <span className="accent">Full Stack Developer</span></a></h1>
             </div>
 
             <ul className="navbar-links">
