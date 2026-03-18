0fb10a89be47995cd08b7c63010f1a41c8a5f323
Joaco004 <jgarinei@hotmail.com>
2026-03-18 17:25:34 -0300

    fix: corrijo errores del navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,10 +4,26 @@ import '../styles/Navbar.css'
 
 const Navbar = () => {
     return(
-        <div className="navbar-logo">
-            <img src={logo} alt="logo" />
-            <h1> Joaquin Garinei <span className="accent">Full Stack Developer</span></h1>
-        </div>
+        <nav className="navbar">
+            <div className="navbar-logo">
+                <img src={logo} alt="logo" />
+                <h1> Joaquin Garinei <span className="accent">Full Stack Developer</span></h1>
+            </div>
+
+            <ul className="navbar-links">
+                <li><a href="#inicio">Inicio</a></li>
+                <li><a href="#sobre-mi">Sobre mí</a></li>
+                <li><a href="#skills">Skills</a></li>
+                <li><a href="#proyectos">Proyectos</a></li>
+                <li><a href="#contacto">Contacto</a></li>
+            </ul>
+
+            <div className="navbar-bottons">
+                <button className="btt-cv">Descargar CV</button>
+                <button className="btt-contact">Contactarme</button>
+            </div>
+        </nav>
+        
     )
 }
 
