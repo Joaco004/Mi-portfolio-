93b20c6c78b8254ea022a0704e9e71517ce840c1
Joaco004 <jgarinei@hotmail.com>
2026-03-19 07:12:24 -0300

    style: agregado de stilos de Projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,48 +1,65 @@
 import '../styles/Proyects.css'
+import { useNavigate } from 'react-router-dom'
 import tecnostore from '../assets/tecnostore.png'
 import jaypee from '../assets/jaypee.png'
 
 
-const proyectos = [
+ export const proyectos = [
     {
         id: 1,
         state: "Terminado",
         img: tecnostore,
-        titulo: "",
+        titulo: "Tecnostore",
         descripcion: "",
+        descripcionLarga: "",
         techs: [],
+        link: "",
+        deploy: ""
     },
     {
         id: 2,
         state: "En curso",
         img: jaypee,
-        titulo: "",
+        titulo: "Jaypee",
         descripcion: "",
+        descripcionLarga: "",
         techs: [],
+        link: "",
+        deploy: ""
     }
 
 ]
 
 const Projects = () => {
+    const navigate = useNavigate()
+
     return (
         <section id="projects" className="projects">
-            {proyectos.map((proyecto) => (
-                <div className='project-box' key={proyectos.id}>
-                    <div className="project-img">
-                        <img src={proyecto.img} alt={proyecto.titulo} />
-                    </div>
-                    <div className="prject-info">
-                        <span className="project-state">{proyecto.state}</span>
-                        <h3>{proyecto.titulo}</h3>
-                        <p>{proyecto.descripcion}</p>
-                        <div className="project-techs">
-                            {proyecto.techs.map((tech, index) => (
-                                <span key={index}>{techs}</span>
-                            ))}
+            <div className="titulo">
+                <h1>PORTFOLIO</h1>
+                <h2>Proyectos</h2>
+                <p>Estas son algunos de los proyectos que ya hice o estoy haciendo</p>
+            </div>
+            <div className="projects-grid">
+                {proyectos.map((proyecto) => (
+                    <div className='project-box' key={proyecto.id} onClick={() => navigate(`/proyecto/${proyecto.id}`)}>
+                        <div className="project-img">
+                            <img src={proyecto.img} alt={proyecto.titulo} />
+                        </div>
+                        <div className="prject-info">
+                            <span className="project-state">{proyecto.state}</span>
+                            <h3>{proyecto.titulo}</h3>
+                            <p>{proyecto.descripcion}</p>
+                            <button className="btn-detalle">Ver mas detalles</button>
+                            <div className="project-techs">
+                                {proyecto.techs.map((tech, index) => (
+                                    <span key={index}>{tech}</span>
+                                ))}
+                            </div>
                         </div>
                     </div>
-                </div>
-            ))}
+                ))}
+            </div>
         </section >
     )
 }
