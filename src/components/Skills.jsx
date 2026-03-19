ea9d218acebc0717cf86778fa06bde5c8710849b
Joaco004 <jgarinei@hotmail.com>
2026-03-19 00:51:03 -0300

    feat: agrego componente skills

---
+++ src/components/Skills.jsx
@@ -0,0 +1,16 @@
+import '../styles/Skills.css'
+import { FaReact, FaHtml5, FaCss3Alt, FaGitAlt, FaGithub } from 'react-icons/fa'
+import { SiJavascript, SiTypescript, SiTailwindcss } from 'react-icons/si'
+import { FaNodeJs } from 'react-icons/fa'
+import { SiExpress, SiMongodb, SiPostgresql, SiMysql } from 'react-icons/si'
+import { SiVite, SiPostman, SiVercel } from 'react-icons/si'
+
+const Skills = () => {
+    return(
+        <section id="skills" className="skills">
+
+        </section>
+    )
+}
+
+export default Skills;
