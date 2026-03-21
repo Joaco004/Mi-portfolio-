072ee9219e85ba12222141b11c121428fadc8a0a
Joaco004 <jgarinei@gmail.com>
2026-03-21 04:46:56 -0300

    feat: agrego componente Contact y estilos de Contact

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
                 <li><a href="#about">Sobre mí</a></li>
                 <li><a href="#skills">Skills</a></li>
                 <li><a href="#projects">Proyectos</a></li>
-                <li><a href="#contacto">Contacto</a></li>
+                <li><a href="#contact">Contacto</a></li>
             </ul>
 
             <div className="navbar-bottons">
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import Hero from '../components/Hero'
 import About from '../components/About'
 import Skills from '../components/Skills'
 import Projects from '../components/Projects'
+import Contact from '../components/Contact'
 
 const Home = () => {
   return (
@@ -10,6 +11,7 @@ const Home = () => {
       <About />
       <Skills />
       <Projects />
+      <Contact />
     </>
   )
 }
